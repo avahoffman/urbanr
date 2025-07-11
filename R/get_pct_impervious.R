83cb0273c937889f28a728e6e93cc77ae00c6263
avahoffman <avamariehoffman@gmail.com>
2025-07-11 16:23:16 -0400

    Update get_pct_impervious.R

--- R/get_pct_impervious.R
+++ R/get_pct_impervious.R
@@ -7,28 +7,37 @@
 #' @export
 #'
 #' @examples
-download_or_check <- function(relative_dir, filename) {
-  data_dir <- paste0("urbanr_data/", relative_dir)
-  file_dir <- paste0(data_dir, "/", filename)
-  url <- paste0("https://raw.githubusercontent.com/avahoffman/urbanr_data/main/", relative_dir, "/", filename)
+download_or_check_impervious <- function(edition = "Annual_NLCD_FctImp_2024_CU_C1V1.tif") {
+  data_dir <- paste0("urbanr_data")
+  file_dir <- paste0(data_dir, "/", edition)
   if (!dir.exists(data_dir)) {
     dir.create(data_dir, recursive = T)
   }
+  # Backup file https://figshare.com/articles/dataset/urbanr_data_Annual_NLCD_FctImp_2024/29549666?file=56194733
   if (!file.exists(file_dir)){
-    # TODO error for bad url. error for no internet connection
-    download.file(url, file_dir)
+    cli::cli_alert(cli::col_cyan(
+      "Data not found! Please first download the data from ",
+      cli::style_hyperlink("National Land Cover Database (NLCD)", "https://www.mrlc.gov/downloads/sciweb1/shared/mrlc/data-bundles/Annual_NLCD_FctImp_2024_CU_C1V1.zip"),
+      ". You can search for more snapshots ",
+      cli::style_hyperlink("here", "https://www.mrlc.gov/data?f%5B0%5D=category%3AFractional%20Impervious%20Surface"),
+      "."
+    ))
+    cli::cli_alert(cli::col_cyan(
+      "Next, add the .tif file to your working directory urbanr_data folder (",
+      paste0(getwd(), "/urbanr_data"),
+      ")."
+    ))
   }
 }
 
 
-get_pct_impervious <- function(latlon){
-  relative_dir <- "NLCD_2016_Impervious"
-  filename <- "NLCD_2016_Impervious_L48_20190405.img"
-
+get_pct_impervious <- function(latlon, edition = "Annual_NLCD_FctImp_2024_CU_C1V1.tif"){
+  data_dir <- paste0("urbanr_data")
+  file_dir <- paste0(data_dir, "/", edition)
   # Get the necessary data
-  download_or_check(relative_dir, filename)
-  data_source <- paste0(here::here(), "/urbanr_data/", relative_dir, "/", filename)
-  r <- terra::rast(data_source)
+  # TODO Error out if not correct dataset
+  download_or_check_impervious(edition = edition)
+  r <- terra::rast(file_dir)
   the_crs <- terra::crs(r)
 
   if(is.numeric(latlon)){
@@ -36,10 +45,11 @@ get_pct_impervious <- function(latlon){
     vals <- data.frame(lon = latlon[2], lat = latlon[1])
     vals_terra <- terra::vect(vals, crs = "+proj=longlat", keepgeom=T)
     extracted_vals <- terra::extract(r, terra::project(vals_terra, the_crs))
+    #TODO nicer output
   }
 
+  return(extracted_vals)
 }
 
-
-  latlon <- c(39.640019, -76.756564)
-  # Possible to download files directly from figshare: eg download.file("https://figshare.com/ndownloader/files/12919973", "downloaded.csv")
+download_or_check_impervious()
+get_pct_impervious(c(39.458686, -76.635277), edition = "Annual_NLCD_FctImp_2024_CU_C1V1.tif")
