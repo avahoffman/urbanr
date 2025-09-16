eb3350aa276006c12e30a29cc251f5c6fa3451ea
avahoffman <avamariehoffman@gmail.com>
2025-09-16 14:42:03 -0400

    Correct testing dirs

--- tests/testthat/test-get_pct_impervious.R
+++ tests/testthat/test-get_pct_impervious.R
@@ -1,4 +1,8 @@
 test_that("dataframe_with_several_cols_ok", {
+  # create data dir
+  dir.create("urbanr_data")
+  
+  ## Compare
   expect_equal(
     get_pct_impervious(
       data.frame(
@@ -8,40 +12,43 @@ test_that("dataframe_with_several_cols_ok", {
         extra2 = "hello"
       ),
       edition = "test_2024.tif",
-      data_dir = "tests/testthat/test_data"
+      data_dir = "fixtures"
     ),
-  get_pct_impervious(
-    data.frame(
-      lat = 39.458686,
-      lon = -76.635277
-    ),
-    edition = "test_2024.tif",
-    data_dir = "tests/testthat/test_data"
+    get_pct_impervious(
+      data.frame(lat = 39.458686, lon = -76.635277),
+      edition = "test_2024.tif",
+      data_dir = "fixtures"
+    )
   )
-  )
+  
+  # Cleanup
+  if (dir.exists("urbanr_data"))
+    unlink("urbanr_data", recursive = TRUE)
 })
 
 test_that("different_editions_different_colnames", {
-  expect_equal(
-    colnames(get_pct_impervious(
-      data.frame(
-        lat = 39.458686,
-        lon = -76.635277
-      ),
+  # create data dir
+  dir.create("urbanr_data")
+  
+  ## Compare
+  expect_equal(colnames(
+    get_pct_impervious(
+      data.frame(lat = 39.458686, lon = -76.635277),
       edition = "test_1988.tif",
-      data_dir = "tests/testthat/test_data"
-    ))[3],
-    "Annual_NLCD_FctImp_1988_CU_C1V1"
-  )
-  expect_equal(
-    colnames(get_pct_impervious(
-      data.frame(
-        lat = 39.458686,
-        lon = -76.635277
-      ),
+      data_dir = "fixtures"
+    )
+  )[3],
+  "Annual_NLCD_FctImp_1988_CU_C1V1")
+  expect_equal(colnames(
+    get_pct_impervious(
+      data.frame(lat = 39.458686, lon = -76.635277),
       edition = "test_2024.tif",
-      data_dir = "tests/testthat/test_data"
-    ))[3],
-    "Annual_NLCD_FctImp_2024_CU_C1V1"
-  )
+      data_dir = "fixtures"
+    )
+  )[3],
+  "Annual_NLCD_FctImp_2024_CU_C1V1")
+  
+  # Cleanup
+  if (dir.exists("urbanr_data"))
+    unlink("urbanr_data", recursive = TRUE)
 })
